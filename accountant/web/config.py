18ea0bb45389f3f7e73d2365c4304768c1cc6ea5
Matej Kupciha <kupciham@hotmail.com>
2021-01-15 11:10:43 +0100

    Read config from environment variables

--- accountant/web/config.py
+++ accountant/web/config.py
@@ -1,3 +1,10 @@
-PORT = 8080
+import os
+
+
+def get_env_int(variable: str, default: int) -> int:
+    return int(os.getenv(variable, default))
+
+
+PORT = get_env_int("PORT", 80)
 UPLOAD_BUCKET_NAME = "accountant-document-uploads"
 RESULT_BUCKET_NAME = "accountant-document-results"
