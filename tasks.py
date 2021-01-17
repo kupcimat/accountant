3e330e7100b7088a201c511a6496dccbc46c41ab
Matej Kupciha <kupciham@hotmail.com>
2021-01-17 16:11:17 +0100

    Push images to ECR repository

--- tasks.py
+++ tasks.py
@@ -1,6 +1,9 @@
 from invoke import task
 
 
+ECR_REPOSITORY = "434425786344.dkr.ecr.us-east-2.amazonaws.com"
+
+
 @task
 def upgrade_deps(ctx):
     """
@@ -23,8 +26,10 @@ def build_images(ctx):
     """
     Build application docker images
     """
-    ctx.run("docker build --file Dockerfile-web --tag kupcimat/accountant-web .")
-    ctx.run("docker build --file Dockerfile-worker --tag kupcimat/accountant-worker .")
+    ctx.run("docker build --file Dockerfile-web --tag accountant-web .")
+    ctx.run("docker build --file Dockerfile-worker --tag accountant-worker .")
+    ctx.run(f"docker tag accountant-web {ECR_REPOSITORY}/accountant-web:latest")
+    ctx.run(f"docker tag accountant-worker {ECR_REPOSITORY}/accountant-worker:latest")
 
 
 @task(build_images)
@@ -32,5 +37,5 @@ def push_images(ctx):
     """
     Build application docker images and push them to docker registry
     """
-    ctx.run("docker push kupcimat/accountant-web")
-    ctx.run("docker push kupcimat/accountant-worker")
+    ctx.run(f"docker push {ECR_REPOSITORY}/accountant-web:latest")
+    ctx.run(f"docker push {ECR_REPOSITORY}/accountant-worker:latest")
