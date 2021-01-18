bdb722e8b255a51a617538cd213ed3e7f133658e
Matej Kupciha <kupciham@hotmail.com>
2021-01-18 09:02:31 +0100

    Move message processing to worker file

--- accountant/queue.py
+++ accountant/queue.py
@@ -3,10 +3,6 @@ import logging
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
-import boto3
-
-from accountant.config import QUEUE_NAME
-
 
 @dataclass
 class Message:
@@ -16,24 +12,12 @@ class Message:
     message_attributes: Dict[str, Any]
 
 
-def process_message():
-    sqs = boto3.client("sqs")
-    queue_url = _get_queue_url(sqs, QUEUE_NAME)
-
-    message = _receive_message(sqs, queue_url)
-    if message:
-        _delete_message(sqs, queue_url, message.receipt_handle)
-        logging.info(f"action=process_message status=success message={message}")
-    else:
-        logging.info("action=process_message status=success message=no_message")
-
-
-def _get_queue_url(sqs, queue_name: str) -> str:
+def get_queue_url(sqs, queue_name: str) -> str:
     response = sqs.get_queue_url(QueueName=queue_name)
     return response["QueueUrl"]
 
 
-def _receive_message(sqs, queue_url: str) -> Optional[Message]:
+def receive_message(sqs, queue_url: str) -> Optional[Message]:
     response = sqs.receive_message(
         QueueUrl=queue_url,
         MaxNumberOfMessages=1,
@@ -47,9 +31,16 @@ def _receive_message(sqs, queue_url: str) -> Optional[Message]:
         if "ObjectCreated:Post" in message["Body"]:
             return _parse_message(message)
         else:
-            _delete_message(sqs, queue_url, receipt_handle)
+            delete_message(sqs, queue_url, receipt_handle)
             logging.warn("action=process_message unknown message")
     return None
+
+
+def delete_message(sqs, queue_url: str, receipt_handle: str):
+    sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle,
+    )
 
 
 def _parse_message(message: Dict[str, Any]) -> Message:
@@ -61,10 +52,3 @@ def _parse_message(message: Dict[str, Any]) -> Message:
         message["MessageAttributes"] if "MessageAttributes" in message else {}
     )
     return Message(s3_object_name, receipt_handle, attributes, message_attributes)
-
-
-def _delete_message(sqs, queue_url: str, receipt_handle: str):
-    sqs.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle,
-    )
--- worker.py
+++ worker.py
@@ -1,11 +1,26 @@
 import logging
 
-from accountant.queue import process_message
+import boto3
+
+from accountant.config import QUEUE_NAME
+from accountant.queue import delete_message, get_queue_url, receive_message
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(filename)s:%(lineno)s %(message)s",
 )
+
+
+def process_message():
+    sqs = boto3.client("sqs")
+    queue_url = get_queue_url(sqs, QUEUE_NAME)
+
+    message = receive_message(sqs, queue_url)
+    if message:
+        delete_message(sqs, queue_url, message.receipt_handle)
+        logging.info(f"action=process_message status=success message={message}")
+    else:
+        logging.info("action=process_message status=success message=no_message")
 
 
 if __name__ == "__main__":
