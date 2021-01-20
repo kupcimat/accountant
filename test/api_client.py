164b674ed34a8d22819bf6390e1ce817de09088a
Matej Kupciha <kupciham@hotmail.com>
2021-01-20 08:56:38 +0100

    Add poll method for api client

--- test/api_client.py
+++ test/api_client.py
@@ -1,12 +1,24 @@
 import asyncio
+import time
 from dataclasses import dataclass
-from typing import Any, Dict
+from typing import Any, Callable, Dict, TypeVar
 
 from aiohttp import ClientSession
 
 
+T = TypeVar("T")
 # TODO make param
 HOST = "http://localhost:8080"
+
+
+def poll(
+    callable: Callable[[], T], condition: Callable[[T], bool], wait_time: int = 2
+) -> T:
+    result = callable()
+    while not condition(result):
+        time.sleep(wait_time)
+        result = callable()
+    return result
 
 
 @dataclass
